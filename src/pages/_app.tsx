a1e44d2f7ee6e5b68b0561e1453d8fe4e9ff3f18
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-16 14:44:19 -0300

    refactor: update website title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Marcos Mendes ⚡️ Links</title>
+        <title>Marcos Mendes | Links úteis</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
